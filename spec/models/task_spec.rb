d1b0897078597a1cb60d020ec6c257c56f465c84
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-03-27 15:42:27 +0300

    Add test for returning tasklist

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "task retrieval" do
+  it "returns a list of all tasks with status and body" do
+    query = <<~GQL
+        {
+        tasks{
+          body
+          state
+        }
+        }
+    GQL
+    expected_result ='{
+      "data": {
+        "tasks": [
+          {
+            "body": "do something",
+            "state": "active"
+          },
+          {
+            "body": "do something else",
+            "state": "completed"
+          },
+          {
+            "body": "do something else else",
+            "state": "active"
+          }
+        ]
+      }
+    }'
+    result =ToDoAppApiGraphqlSchema.execute(query)
+    pp result
+    expect(result).equal?(expected_result)
+  end
+
+
 end
