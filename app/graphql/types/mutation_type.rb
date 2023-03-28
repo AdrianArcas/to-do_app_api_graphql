de79e8c7d4e9127ab09db047ab8f13abf1863025
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-03-28 12:21:01 +0300

    Add the edit functionality

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
 
     field :create_task, resolver: Resolvers::CreateTask
     field :check_task, resolver: Resolvers::CheckTask
+    field :edit_task,resolver: Resolvers::EditTask
   end
 end
