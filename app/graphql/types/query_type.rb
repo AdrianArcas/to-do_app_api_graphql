a9c057ce759adc884dfc7e03dd61971bd5bbbd32
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-03-28 13:38:47 +0300

    Add a query that returns the no of active tasks

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,5 +2,6 @@ module Types
   class QueryType < Types::BaseObject
     field :tasks, resolver: Resolvers::GetTasks
     field :active_tasks, resolver: Resolvers::GetActiveTasks
+    field :active_tasks_counter, resolver: Resolvers::GetActiveTasksCounter
   end
 end
