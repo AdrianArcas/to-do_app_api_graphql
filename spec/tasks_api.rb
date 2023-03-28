c621862a180a6fa498bf068a154b8a99f1c14767
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-03-28 15:25:45 +0300

    Add test to delete all completed tasks mutation

--- spec/tasks_api.rb
+++ spec/tasks_api.rb
@@ -1,79 +1,79 @@
 require 'rails_helper'
 
 def list_tasks
-    query = <<~GQL
-        {
-        tasks{
-          body
-          state
-        }
-        }
-    GQL
+  query = <<~GQL
+    {
+    tasks{
+      body
+      state
+    }
+    }
+  GQL
 
-    post graphql_path, params: { query: query }
+  post graphql_path, params: { query: query }
 
-    JSON.parse(response.body)
+  JSON.parse(response.body)
 end
 
 def create_task(body)
-    query = <<~GQL
-        mutation CreateTask($body : String!){
-        createTask(
-        body: $body
-        )
-        { id
-          body
-          state
-	      }
-      }
-    GQL
-    variables = {body: body}
+  query = <<~GQL
+      mutation CreateTask($body : String!){
+      createTask(
+      body: $body
+      )
+      { id
+        body
+        state
+    	      }
+    }
+  GQL
+  variables = { body: body }
 
-    post graphql_path, params: { query: query, variables: variables }
+  post graphql_path, params: { query: query, variables: variables }
 
-    JSON.parse(response.body)
-  end
+  JSON.parse(response.body)
+end
 
 def check_task(id)
   query = <<~GQL
-        mutation CheckTask($id : ID!){
-        checkTask(id: $id)
-        {
-          id
-          body
-          state
-	      }
-      }
+      mutation CheckTask($id : ID!){
+      checkTask(id: $id)
+      {
+        id
+        body
+        state
+    	      }
+    }
   GQL
-  variables ={id: id}
+  variables = { id: id }
   post graphql_path, params: { query: query, variables: variables }
   JSON.parse(response.body)
 end
 
-def edit_task(id,body)
+def edit_task(id, body)
   query = <<~GQL
-        mutation EditTask($id : ID!,$body : String!){
-        editTask(id: $id,body: $body)
-        {
-          id
-          body
-          state
-	      }
-      }
+      mutation EditTask($id : ID!,$body : String!){
+      editTask(id: $id,body: $body)
+      {
+        id
+        body
+        state
+    	      }
+    }
   GQL
-  variables ={id: id,body:body}
+  variables = { id: id, body: body }
   post graphql_path, params: { query: query, variables: variables }
   JSON.parse(response.body)
 end
 
 def list_active_tasks
   query = <<~GQL
-        {
-        activeTasks{
-          body
-          state
-        }
-        }
+    {
+    activeTasks{
+      body
+      state
+    }
+    }
   GQL
 
   post graphql_path, params: { query: query }
@@ -83,9 +83,23 @@ end
 
 def active_tasks_counter
   query = <<~GQL
-        {
-        activeTasksCounter
+    {
+    activeTasksCounter
+    }
+  GQL
+
+  post graphql_path, params: { query: query }
+
+  JSON.parse(response.body)
+end
+
+def delete_completed_tasks
+  query = <<~GQL
+  mutation {
+        deleteCompletedTasks {
+              id
         }
+    }
   GQL
 
   post graphql_path, params: { query: query }
