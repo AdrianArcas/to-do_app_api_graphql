0d3e95c52977ab6fea589b8e2167cae27b208e72
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-03-28 15:25:18 +0300

    Add functionality to delete all completed tasks

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
     field :create_task, resolver: Resolvers::CreateTask
     field :check_task, resolver: Resolvers::CheckTask
     field :edit_task,resolver: Resolvers::EditTask
+    field :delete_completed_tasks,resolver: Resolvers::DeleteCompletedTasks
   end
 end
--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -2,5 +2,6 @@ module Types
   class TaskType < Types::BaseObject
     field :body, String, null: false
     field :state, String, null: false
+    field :id,ID,null:false
   end
 end
