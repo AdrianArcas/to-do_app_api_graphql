8afe685ab208382eb65e75fc8c8101a6b0c02b03
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-03-28 15:44:42 +0300

    Add functionality to uncheck a checked task

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@ module Types
 
     field :create_task, resolver: Resolvers::CreateTask
     field :check_task, resolver: Resolvers::CheckTask
+    field :undo_check_task, resolver: Resolvers::UndoCheckTask
     field :edit_task,resolver: Resolvers::EditTask
     field :delete_completed_tasks,resolver: Resolvers::DeleteCompletedTasks
   end
