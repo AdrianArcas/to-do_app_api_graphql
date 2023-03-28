ba7083856245eff49544f799c6af3f1edd10c8b1
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-03-28 11:24:06 +0300

    Refactor tests

--- spec/integration/add_task_spec.rb
+++ spec/integration/add_task_spec.rb
@@ -1,24 +1,18 @@
 require 'rails_helper'
+require 'tasks_api'
 
 RSpec.describe 'create_task', type: :integration  do
 
   it "creates a new task " do
-    query = <<~GQL
-        mutation{
-        createTask(
-        body: "tested task"
-                  )
-        {
-          body
-          state
-	      }
-      }
-    GQL
-    expected_result = Task.last
+    tasks = list_tasks
 
-    post graphql_path, params: { query: query }
+    expect(tasks['data']).to eq "tasks" => []
 
-    parsed_result = JSON.parse(response.body)
-    expect(parsed_result).equal?(expected_result)
+    create_task("Buy Milk")
+    create_task("Make food")
+    tasks = list_tasks
+
+    expect(tasks['data']).to eq "tasks"=>[{ 'body' => 'Buy Milk', 'state' => 'active'}, {'body' => 'Make food', 'state' => 'active'}]
+
   end
 end
